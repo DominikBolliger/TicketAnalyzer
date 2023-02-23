9bbcd9bedf8558f43c06bf868b46e33c851a1660
Nik181 <N_bolliger45@hotmail.com>
2023-02-23 10:11:14 +0100

    first

--- src/controller/TicketController.java
+++ src/controller/TicketController.java
@@ -1,9 +1,12 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import logic.XMLReader;
 import modell.Ticket;
@@ -14,6 +17,7 @@ import java.util.List;
 public class TicketController {
     private XMLReader reader;
     private List<Button> buttonsList;
+    private List<TableColumn> columnList;
     @FXML
     protected HBox hBoxPages;
     @FXML
@@ -23,6 +27,20 @@ public class TicketController {
         buttonsList = new ArrayList<>();
         reader = new XMLReader();
         reader.readNodes();
+        createButtons();
+        buttonsList.get(0).fire();
+        populateTableView();
+    }
+
+    private void setUpPage(String pageName) {
+        tblvTickets.getColumns().clear();
+        for (String columnName:reader.getColumnNames(pageName)){
+            columnList.add(new TableColumn(columnName));
+        }
+        tblvTickets.getColumns().addAll(columnList);
+    }
+
+    private void createButtons(){
         for (String pageName:reader.returnPageNames()){
             Button button = new Button(pageName);
             buttonsList.add(button);
@@ -31,16 +49,15 @@ public class TicketController {
             });
             hBoxPages.getChildren().add(button);
         }
-        buttonsList.get(0).fire();
-        for (Ticket ticket:Ticket.getTicketList()){
-            System.out.println(ticket);
-        }
     }
 
-    private void setUpPage(String pageName) {
-        tblvTickets.getColumns().clear();
-        for (String columnName:reader.getColumnNames(pageName)){
-            tblvTickets.getColumns().add(new TableColumn(columnName));
+    private void populateTableView() {
+        for (TableColumn column: columnList){
+            column.setCellValueFactory(new PropertyValueFactory<>(column.getText()));
         }
+        tblvTickets.setItems(ticketModels);
     }
+
+    private ObservableList<Ticket> ticketModels = FXCollections.observableList(Ticket.getTicketList());
+
 }
